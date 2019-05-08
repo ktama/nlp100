0ccb83f17775b39be931840a852bd805bd75b596
ktama <ktama@users.noreply.github.com>
2019-05-08 21:32:40 +0900

    work00完

---
+++ work00.py
@@ -0,0 +1,18 @@
+# -*- Coding: utf-8 -*-
+
+# 00. 文字列の逆順
+# 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+
+# 参考
+# 配列反転させるだけ
+# https://qiita.com/Hawk84/items/ecd0c7239e490ea22308
+
+
+def reverse(target_str=''):
+    return target_str[::-1]
+
+if __name__ == '__main__':
+    result = reverse('stressed')
+    print(result)
+
+    
