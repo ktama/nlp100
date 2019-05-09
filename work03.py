1854fe4e5fbcb29494f016228bc41c7c28f43adf
ktama <ktama@users.noreply.github.com>
2019-05-09 23:48:53 +0900

    work04 + modify work03

--- work03.py
+++ work03.py
@@ -4,10 +4,9 @@
 # "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
 
 def count_alphabet(target_str=''):
-    words = target_str.replace(',', '').replace('.', '').split(' ')
-    return map(lambda x: len(x), words)
+    words = target_str.strip(',.').split(' ')
+    return list(map(lambda x: len(x), words))
 
 if __name__ == '__main__':
     count_list = count_alphabet('Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.')
-    for count in count_list:
-        print(count)
+    print(count_list)
---
+++ work04.py
@@ -0,0 +1,22 @@
+# -*- Coding: utf-8 -*-
+
+# 04. 元素記号
+# "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."という文を単語に分解し，
+# 1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，
+# それ以外の単語は先頭に2文字を取り出し，
+# 取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
+
+def count_dic(target_str=''):
+    words = target_str.strip(',.').split(' ')
+    dic = {}
+    for index, word in enumerate(words, 1):
+        if index in (1,5,6,7,8,9,15,16,19):
+            dic[word[:1]] = index
+        else:
+            dic[word[:2]] = index
+
+    return dic
+
+if __name__ == '__main__':
+    dic = count_dic('Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.')
+    print(dic)
